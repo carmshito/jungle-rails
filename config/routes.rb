9ac4a191b0fb962296138ad7b8b5569668083309
Carmelia Shito <carmshito@hotmail.com>
2023-10-01 23:03:53 -0400

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'about/index'
   root to: 'products#index'
+  
+  resources :about, only: [:index]
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -9,6 +12,7 @@ Rails.application.routes.draw do
     post   :add_item
     post   :remove_item
   end
+
 
   resources :orders, only: [:create, :show]
 
